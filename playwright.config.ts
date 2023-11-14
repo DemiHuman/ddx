e4fc526e732106079c8ce6282db606cea4c5ab85
Pavel <eropkinp@gmail.com>
2023-11-14 17:37:21 +0300

    added project

---
+++ playwright.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests',
+  fullyParallel: true,
+  reporter: 'html',
+  use: {
+    trace: 'off',
+  },
+
+  /* Configure projects for major browsers */
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    }
+  ],
+});
---
+++ tests/first.test.ts
@@ -0,0 +1,7 @@
+import { test } from '@playwright/test';
+
+test.describe("Группа тестовых тестов", async () => {
+    test("My first test",async () => {
+        console.log("My first test! (^_^)");     
+    });
+});
