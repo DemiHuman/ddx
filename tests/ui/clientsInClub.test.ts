5b98d28bf7622d04887a12caee82788780793c7f
Pavel <eropkinp@gmail.com>
2023-11-27 20:17:36 +0300

    Добавлен тест 'Клиенты в клубе'

--- tests/ui/clientsInClub.test.ts
+++ tests/ui/clientsInClub.test.ts
@@ -3,8 +3,8 @@ import authCRMTestData from "@data/authCRM.json"
 import crmUrl from "api.json"
 
 test.use({headless: false})
-test.describe("Тесты на авторизацию в CRM", async () => {
-    test("Успешная авторизация в CRM", async ({ page}) => {
+test.describe("Тесты на пункт меню 'Клиенты в клубе'", async () => {
+    test("Успешный переход на пункт меню 'Клиенты в клубе'", async ({ page}) => {
         await test.step("Перейти на страницу входа в CRM", async () => {
             await page.goto(crmUrl.urls.base_url_crm);
         });
@@ -16,9 +16,12 @@ test.describe("Тесты на авторизацию в CRM", async () => {
             await page.getByRole("button", { name: 'Войти'}).click();
         });
 
-        await test.step("Заполнить форму авторизации и нажать найти", async () => {
-            // Пример работы с xpath
-            await page.locator("//input[@data-testid='phone-input']").waitFor({state: 'visible', timeout: 3000});
+        await test.step("Нажать на пункт меню 'Клиенты в клубе'", async () => {
+            await page.getByText("Клиенты в клубе").click();
+        });
+
+        await test.step("Проверть отобржение надписи 'Сейчас в клубе'", async () => {
+            await page.getByText("Сейчас в клубе").waitFor({state: 'visible', timeout: 3000});
         });
     });
 })
